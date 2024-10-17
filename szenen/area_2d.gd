ec4c76c54afed46fdf5c741459cef49e3a77bfa7
Ililja <Ilja09@outlook.de>
2024-10-17 18:26:32 +0200

    Update area_2d.gd

--- szenen/area_2d.gd
+++ szenen/area_2d.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@onready var body: CharacterBody2D = $CharacterBody2D
+@onready var body1: CharacterBody2D = $CharacterBody2D
 @onready var player: Node2D = $"../player"
 
 # Called when the node enters the scene tree for the first time.
@@ -17,5 +17,4 @@ func _on_body_entered(body: Node2D) -> void:
 	player.position.x = -800
 	player.position.y = 2
 	body.stop()
-	print ("teleport")  # Replace with function body.
 	
