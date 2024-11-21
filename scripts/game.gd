22ac21677f6cb9e4ea7adcd879b9d0e8f98a4c9b
Wassertester <raphaelunbekannt2@gmail.com>
2024-11-21 19:39:38 +0100

    checkpoints

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,7 +2,9 @@ extends Node2D
 var menu_fragezeichen = false
 @onready var resume: Button = $Settings/MarginContainer/VBoxContainer/resume
 @onready var start_island_cam: Camera2D = $"start island/start_island_cam"
+@onready var player: Node2D = $player/player_script
 signal camera
+var respawn_cords = Vector2(-800, 2)
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("escape"):
@@ -10,7 +12,7 @@ func _input(event: InputEvent) -> void:
 			camera.emit("menu")
 		else:
 			camera.emit("player")
-
+# pauses game when menu is opened
 func _on_camera(str) -> void:
 	if str == "menu":
 		Engine.time_scale = 0
@@ -23,3 +25,14 @@ func _on_camera(str) -> void:
 		start_island_cam.enabled = true
 	else:
 		start_island_cam.enabled = false
+
+func _on_checkpoints_checkpoint_reached(Position: Vector2) -> void:
+	respawn_cords = Position
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	camera.emit("player")
+	respawn()
+	
+func respawn():
+	player.position = respawn_cords
+	player.stop()
---
+++ szenen/checkpoints.gd
@@ -0,0 +1,14 @@
+extends Node2D
+@onready var checkpoint_1: Area2D = $checkpoint1
+signal checkpoint_reached(Position: Vector2)
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_checkpoint_1_body_entered(body: Node2D) -> void:
+	checkpoint_reached.emit(checkpoint_1.position)
