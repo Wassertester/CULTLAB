a1d71f3bb92e31362f6cc48ffa3d02262a47298e
Wassertester <raphaelunbekannt2@gmail.com>
2024-12-07 12:17:39 +0100

    pretty checkpoints

--- scripts/checkpoint.gd
+++ scripts/checkpoint.gd
@@ -2,8 +2,17 @@ extends Area2D
 @onready var cpu_particles_2d: CPUParticles2D = $CPUParticles2D
 @onready var game: Node2D = $"../.."
 @onready var checkpoints: Node2D = $"."
+@onready var flag: Sprite2D = $flag
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 func _on_body_entered(body: Node2D) -> void:
 	if game.save.respawn_cords != global_position + Vector2(0, 50):
-		print(global_position)
-		print(game.save.respawn_cords)
+		animation_player.play("get checkpoint")
 		cpu_particles_2d.emitting = true
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	if game.save.respawn_cords == global_position + Vector2(0, 50):
+		flag.position = Vector2(0, -32)
+	else:
+		flag.position = Vector2(0, 16)
